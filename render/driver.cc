ffa8e636bcc309a6d49781f995c86593e7b06976
Robert Lemmen <robert.lemmen@egym.de>
2019-05-29 15:49:08 +0200

    Skeleton bison/flex parser (from docs)

---
+++ render/driver.cc
@@ -0,0 +1,26 @@
+#include "driver.h"
+#include "parser.hh"
+
+driver::driver(): 
+		trace_parsing (false), 
+		trace_scanning (false) {
+	variables["one"] = 1;
+	variables["two"] = 2;
+}
+
+int driver::parse(const std::string &f) {
+	file = f;
+	location.initialize (&file);
+	scan_begin ();
+	yy::parser parse (*this);
+	parse.set_debug_level (trace_parsing);
+	int res = parse ();
+	scan_end ();
+	return res;
+}
+
+int main(int argc, char **argv) {
+	driver drv;
+	drv.parse("sample");
+	return 0;
+}
